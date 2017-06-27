4377364bf97967d22b0d964b1825c079daed33c1
myj <1368468125@qq.com>
2017-06-27 21:44:53 +0800

    2017年6月27日21:44:52

--- react+vue+webpack.java
+++ react+vue+webpack.java
@@ -4,7 +4,22 @@ react.js
 react-dom.js
 browser.js //解析JSX语法
 
-
+<script type="text/babel">
+	var StudentApp = React.createClass({
+		render: function(){
+			// 注意一个组件里面只能有一个顶级标签，此外像input这样的标签需要闭合
+			// class需要改成className
+			return (
+				<div>
+					<h1 className="list">学生信息表</h1>
+					<input type="text" value="aaa" />
+				</div>
+			);
+		}
+	});
+	// 将StudentApp组件插入到id为studentInfo的元素中
+	ReactDOM.render(<StudentApp />, document.getElementById("studentInfo"));
+</script>
 
 
 
